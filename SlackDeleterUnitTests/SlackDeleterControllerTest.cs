0a342f1b7dc92bc58b1b0573e7602865db03d4cd
Chris Bush <cjbush77@gmail.com>
2018-11-28 04:41:55 -0500

    Added unit tests for controllers

---
+++ SlackDeleterUnitTests/SlackDeleterControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SlackDeleterWeb.Api;
+using SlackDeleterWeb.Model;
+using SlackDeleterWeb.Utilities;
+
+namespace SlackDeleterUnitTests {
+	[TestFixture]
+	public sealed class SlackDeleterControllerTest {
+		private SlackDeleterController subject;
+		private Mock<ISlackHelper> slackHelper;
+
+		[SetUp]
+		public void SetUp() {
+			slackHelper = new Mock<ISlackHelper>();
+			subject = new SlackDeleterController(slackHelper.Object);
+		}
+
+		[Test]
+		public async Task Post_ShouldDeleteFile() {
+			slackHelper.Setup(h => h.DeleteFile("bar", "foo")).Returns(Task.FromResult(new SlackFileDeleteResponse {
+				OK = true
+			})).Verifiable();
+			await subject.Post("foo", "bar");
+			slackHelper.VerifyAll();
+		}
+
+	}
+}
---
+++ SlackDeleterUnitTests/SlackFileListControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SlackDeleterWeb.Api;
+using SlackDeleterWeb.Model;
+using SlackDeleterWeb.Utilities;
+
+namespace SlackDeleterUnitTests {
+	[TestFixture]
+	public sealed class SlackFileListControllerTest {
+
+		private SlackFileListController subject;
+		private Mock<ISlackHelper> slackHelper;
+
+		[SetUp]
+		public void SetUp() {
+			slackHelper = new Mock<ISlackHelper>();
+			subject = new SlackFileListController(slackHelper.Object);
+		}
+
+		[Test]
+		public async Task Get_ShouldReturnValidResponse() {
+			slackHelper.Setup(h => h.RetrieveFilesOlderThan(30, "foo")).Returns(Task.FromResult(new SlackFileListResponse {
+				OK = true,
+				Files = new List<SlackFile> { },
+				Paging = new SlackFileListPaging { }
+			})).Verifiable();
+			await subject.Get("foo", 30);
+			slackHelper.VerifyAll();
+		}
+
+	}
+}
